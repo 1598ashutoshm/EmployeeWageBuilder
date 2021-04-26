e940739131ccf229c3e5153d29ac26f8767aa8d8
1598ashutoshm <82252370+1598ashutoshm@users.noreply.github.com>
2021-04-27 00:39:51 +0530

    Update EmpWageBuilder.java

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -8,7 +8,7 @@ public class EmpWageBuilder {
 		int salary,emphrs;
 		int EMP_RATE_PER_HR = 20;
 		
-		double empCheck = Math.floor(Math.random() * 10) % 3;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == IS_FULL_TIME)
 			emphrs=8;
 		else
