638a07a71fea877e4e7ff35c327954cb43e3a139
1598ashutoshm <ashutoshm1598@gmail.com>
2021-04-27 01:08:34 +0530

    Adding: WageForMonth

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -8,26 +8,31 @@ public class EmpWageBuilder {
 		int IS_PART_TIME= 2;
 		int salary,emphrs;
 		int EMP_RATE_PER_HR = 20;
+		int NO_OF_WORKING_DAYS=20;
+		int totalworkinghours=0;
 		
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-	    int empcheck =(int)empCheck;
-		switch (empcheck) {
-	    case 1:
-	    	emphrs=8;
-	    	break;
-	    case 2:
-	    	emphrs=4;
-	    	break;
-	    default:
-	    	emphrs=0;
-	    	break;
-	    	
-	    }
-            
-		
-		salary=EMP_RATE_PER_HR * emphrs;
+		for(int day=1;day<=NO_OF_WORKING_DAYS;day++)
+		{
+			double empCheck = Math.floor(Math.random() * 10) % 3;
+		    int empcheck =(int)empCheck;
+			switch (empcheck) {
+		    case 1:
+		    	emphrs=8;
+		    	break;
+		    case 2:
+		    	emphrs=4;
+		    	break;
+		    default:
+		    	emphrs=0;
+		    	break;
+		    	
+		    }
+			
+			totalworkinghours= totalworkinghours + emphrs; 
+		}
+	
+		salary=EMP_RATE_PER_HR * totalworkinghours;
 		System.out.print(salary);
-		
-		
+			
 	}
 }
