7b5b1091879d88ee3cdced69adf848a8e94dd7e1
1598ashutoshm <ashutoshm1598@gmail.com>
2021-04-27 00:38:33 +0530

    Adding: PartTime

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -5,17 +5,20 @@ public class EmpWageBuilder {
 		// TODO Auto-generated method stub
 		
 		int IS_FULL_TIME = 1;
+		int IS_PART_TIME= 2;
 		int salary,emphrs;
 		int EMP_RATE_PER_HR = 20;
 		
 		double empCheck = Math.floor(Math.random() * 10) % 3;
 		if (empCheck == IS_FULL_TIME)
 			emphrs=8;
+		else if (empCheck == IS_PART_TIME)
+			emphrs=4;
 		else
-			emphrs=0;
+		    emphrs=0;
 		
 		salary=EMP_RATE_PER_HR * emphrs;
-		System.out.println(salary);
+		System.out.print(salary);
 		
 		
 	}
