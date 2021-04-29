8cd8db8ddc564c0ec354143a62b5e454fcbfb195
1598ashutoshm <ashutoshm1598@gmail.com>
2021-04-29 20:36:19 +0530

    Adding: OopsConcept

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -3,28 +3,26 @@ public class EmpWageBuilder {
 	
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME= 2;
-	public static final int NO_OF_WORKING_DAYS=20;
-	public static final int EMP_RATE_PER_HR = 2;
-	public static final int max_hrs_in_months=10;
+	public static final int NO_OF_WORKING_DAYS=2;
+	public static final int EMP_RATE_PER_HR = 20;
+	public static final int MAX_HRS_IN_MONTH=10;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		
+
 		int salary,emphrs;
 		int totalworkinghours=0;
 		int totalworkingdays=0;
 		
-		while ((totalworkingdays<NO_OF_WORKING_DAYS) && (totalworkinghours<max_hrs_in_months))
+		while (totalworkingdays<NO_OF_WORKING_DAYS && totalworkinghours<MAX_HRS_IN_MONTH)
 		{
 			totalworkingdays=totalworkingdays+1;
 			double empCheck = Math.floor(Math.random() * 10) % 3;
 			int empcheck =(int)empCheck;
 			switch (empcheck) {
-		    case 1:
+		    case IS_FULL_TIME:
 		    	emphrs=8;
 		    	break;
-		    case 2:
+		    case IS_PART_TIME:
 		    	emphrs=4;
 		    	break;
 		    default:
@@ -35,6 +33,6 @@ public class EmpWageBuilder {
 	        System.out.println("Day#: " + totalworkingdays + " Emp hr" + emphrs);
 		}
 		salary=EMP_RATE_PER_HR * totalworkinghours;
-		System.out.print(salary);		
+		System.out.print("Total Wage: "+ salary);		
 	}
 }
